4e336cdba45cc79d940025e96a4fef78f73094c0
Hai Dun <duonghuynh22092004@gmail.com>
2024-11-06 19:47:48 +0700

    Them SanPhamDAO

--- src/DAO/SanPhamDAO.java
+++ src/DAO/SanPhamDAO.java
@@ -1,7 +1,20 @@
 package DAO;
 
+import java.sql.Statement;
+
+import config.MySQLConnection;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class SanPhamDAO {
     private Connection conn;
+    private Statement stmt;
+    private PreparedStatement pstmt;
+    private ResultSet rs;
+    public SanPhamDAO(){
+        conn = MySQLConnection.getConnection();
+    }
+
 }
