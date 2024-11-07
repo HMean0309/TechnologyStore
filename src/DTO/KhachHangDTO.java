518ba3405c432d7cbea659318d90f1bea557e5ab
HMean0309 <HMean0309@gmail.com>
2024-11-07 19:24:34 +0700

    them giao dien khach hang

---
+++ src/DTO/KhachHangDTO.java
@@ -0,0 +1,127 @@
+
+package DTO;
+
+
+public class KhachHangDTO {
+
+    private String maKH;
+    private String name;
+    private String address;
+    private String district;
+    private String ward;
+    private String city;
+    private String phoneNumber;
+    
+    public KhachHangDTO() {
+    }
+
+    public KhachHangDTO(String maKH, String name, String address, String district, String ward, String city, String phoneNumber) {
+        this.maKH = maKH;
+        this.name = name;
+        this.address = address;
+        this.district = district;
+        this.ward = ward;
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @return the maKH
+     */
+    public String getMaKH() {
+        return maKH;
+    }
+
+    /**
+     * @param maKH the maKH to set
+     */
+    public void setMaKH(String maKH) {
+        this.maKH = maKH;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the district
+     */
+    public String getDistrict() {
+        return district;
+    }
+
+    /**
+     * @param district the district to set
+     */
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    /**
+     * @return the ward
+     */
+    public String getWard() {
+        return ward;
+    }
+
+    /**
+     * @param ward the ward to set
+     */
+    public void setWard(String ward) {
+        this.ward = ward;
+    }
+
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    
+}
--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -14,6 +14,7 @@ import com.formdev.flatlaf.extras.FlatSVGIcon;
 
 public class Main extends javax.swing.JFrame {
 
+    KhachHang khachHang;
     
     Color BackgroundColor = new Color(240, 247, 250);
     public Main() {
@@ -406,6 +407,7 @@ public class Main extends javax.swing.JFrame {
 
     private void btnKhachHangActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnKhachHangActionPerformed
         // TODO add your handling code here:
+        
     }//GEN-LAST:event_btnKhachHangActionPerformed
 
     private void btnNhaCungCapActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNhaCungCapActionPerformed
